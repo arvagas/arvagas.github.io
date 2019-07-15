090ad2ae5b4c36d6a99a0ae61b1dce7f6a3addbb
Arvin Agas <arvagas@gmail.com>
2019-07-15 12:52:45 -0400

    removed instances of document.ready

--- components/header/header.js
+++ components/header/header.js
@@ -1,38 +1,30 @@
 // Change header settings when scrolling past the first section
-$(document).ready(() => {
-    $(window).scroll(() => {
-        let scroll = $(window).scrollTop()
-        let transOne = $(window).height() - $('header').height()
-        if (scroll > transOne) {
-            $('header').addClass('header-trans-one')
-        } else {
-            $('header').removeClass('header-trans-one')
-        }
-    })
+$(window).scroll(() => {
+    let scroll = $(window).scrollTop()
+    let transOne = $(window).height() - $('header').height()
+    if (scroll > transOne) {
+        $('header').addClass('header-trans-one')
+    } else {
+        $('header').removeClass('header-trans-one')
+    }
 })
 
 // Open or close when mobile button is clicked
-$(document).ready(() => {
-    $('.mobile-btn').click(() => {
-        $('.nav-container').slideToggle()
-    })
+$('.mobile-btn').click(() => {
+    $('.nav-container').slideToggle()
 })
 
 
 
 // Close menu when windows resizes
-$(document).ready(() => {
-    $(window).resize(() => {
-        if ($(window).width() > 500) $('.nav-container').show()
-        else $('.nav-container').slideUp()
-    })
+$(window).resize(() => {
+    if ($(window).width() > 500) $('.nav-container').show()
+    else $('.nav-container').slideUp()
 })
 
 
 // Close menu when scroll
-$(document).ready(() => {
-    $(window).scroll(() => {
-        if ($(window).width() > 500) $('.nav-container').show()
-        else $('.nav-container').slideUp()
-    })
+$(window).scroll(() => {
+    if ($(window).width() > 500) $('.nav-container').show()
+    else $('.nav-container').slideUp()
 })
