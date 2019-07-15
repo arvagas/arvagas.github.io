1bf4ce43cc73ff5a53955037114f60b0be266bb9
Arvin Agas <arvagas@gmail.com>
2019-07-14 20:10:09 -0400

    initial work on projects input complete

--- components/projects/projects.js
+++ components/projects/projects.js
@@ -1,25 +1,83 @@
-const projectList = [
+const projectArray = [
     {
         title:'Immunization Tracker',
         description: 'Immunization Tracker appeals to three distinct groups of people: Parents who want to keep track of their children\'s immunization history, adult patients who want to log their own immunization history, and providers who can keep track of their patient\'s records and update them accordingly',
         image:'../images/projects/immunization-tracker.gif',
-        tech:'html;css;javascript;jquery',
-        deploy:'immunization-tracker.netlify.com',
+        tech:'HTML;CSS;JavaScript;jQuery',
+        deploy:'https://immunization-tracker.netlify.com',
         repo:'https://github.com/immunization-tracker/immunization-tracker-UI',
-        responsiblity:'Create a wireframe that will be used as a starting point.;Collaborate with other UI developers on creating a landing page for Immunization Tracker.; a solo page (Help) to demonstrate personal ability.'
+        design: 'https://google.com',
+        responsibility:'Create a wireframe that will be used as a starting point.;Collaborate with other UI developers on creating a landing page for Immunization Tracker.; a solo page (Help) to demonstrate personal ability.'
     },
     {
         title:'Portfolio',
         description: 'A website portfolio for myself',
-        image:'../images/projects/immunization-tracker.gif',
-        tech:'html;css;javascript',
-        deploy:'arvinagas.com',
+        image:'',
+        tech:'HTML;CSS;JavaScript',
+        deploy:'https://arvinagas.com',
         repo:'https://github.com/arvagas/arvagas.github.io',
-        responsiblity:'Utilize the knowledge acquired to create a website marketing myself to show the world what they\'re missing out on'
+        design: null,
+        responsibility:'Utilize the knowledge acquired to create a website marketing myself to show the world what they\'re missing out on'
     },
 ]
 
+const projectsSection = document.querySelector('.projects-container')
 
+projectArray.forEach(project => {
+    projectsSection.appendChild(projectsCard(project))
+})
+
+function projectsCard(projectData) {
+    // Create elements
+    const project = document.createElement('div')
+    const title = document.createElement('h1')
+    const description = document.createElement('h2')
+    const tech = document.createElement('p')
+    const linksBox = document.createElement('div')
+    const deploy = document.createElement('a')
+    const breakOne = document.createElement('p')
+    const repo = document.createElement('a')
+    const breakTwo = document.createElement('p')
+    const design = document.createElement('a')
+    const responsibility = document.createElement('p')
+
+    // Structure elements
+    project.appendChild(title)
+    project.appendChild(description)
+    project.appendChild(tech)
+    project.appendChild(linksBox)
+    linksBox.appendChild(deploy)
+    linksBox.appendChild(breakOne)
+    linksBox.appendChild(repo)
+    linksBox.appendChild(breakTwo)
+    linksBox.appendChild(design)
+    project.appendChild(responsibility)
+
+    // Set attributes
+    title.textContent = projectData.title
+    description.textContent = projectData.description
+    tech.textContent = projectData.tech
+    deploy.textContent = 'Deploy'
+    deploy.href = projectData.deploy
+    breakOne.textContent = ' | '
+    repo.textContent = 'Github'
+    repo.href = projectData.repo
+    breakTwo.textContent = ' | '
+    design.textContent = 'Design'
+    design.href = projectData.design
+    responsibility.textContent = projectData.responsibility
+    
+    // Special case for design property
+    if (projectData.design === null) {
+        breakTwo.style.display = 'none'
+        design.style.display = 'none'
+    }
+
+    // Assign class names
+
+
+    return project
+}
 
 
 // const topicsBar = document.querySelector('.topics')
